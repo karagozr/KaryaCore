0f30904330b30642e6cc9cf30d306efe03e7ecf1
karagozr <karagozr1@gmail.com>
2026-08-01 11:53:23 +0300

    jhjhhhhh

---
+++ Karya.Test.Web.Api/Migrations/20260710121121_tsss.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Karya.Test.Web.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class tsss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "InventoryDetail",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 0)
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "InventoryDetail",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("Relational:ColumnOrder", 0)
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
