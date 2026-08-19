0dc371f3e4066dcfffa8fe2bebf86066e44ca06d
karagozr <karagozr1@gmail.com>
2026-08-19 10:03:41 +0300

    DtoMember for BaseEntity

--- Karya.Core/Abstracts/Entities/BaseEntity.cs
+++ Karya.Core/Abstracts/Entities/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using Karya.Core.Interfaces.Entities;
+﻿using Karya.Core.Common.Attributes.Data;
+using Karya.Core.Interfaces.Entities;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,11 +8,13 @@ namespace Karya.Core.Abstracts.Entities;
 public abstract class BaseEntity<TId> : IBaseEntity<TId>
 {
     [Key, Column(Order = 0)]
+    [DtoMember(DtoTargets.SBIU)]
     public virtual TId Id { get; set; }
 }
 
 public abstract class BaseEntity : BaseEntity<Guid>
 {
+    [DtoMember(DtoTargets.SBIU)]
     [Key, Column(Order = 0)]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     override public Guid Id { get; set; }
