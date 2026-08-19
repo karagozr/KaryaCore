ed9b42933a177f72775070481db5769b229acad3
karagozr <karagozr1@gmail.com>
2026-08-20 00:16:51 +0300

    TenantModelBuilderExtensions and BaseDbContext added

---
+++ Karya.Core/Abstracts/Contexts/BaseDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Karya.Core.Common.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Karya.Core.Abstracts.Contexts;
+
+public abstract class BaseDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ConfigureTenantForeignKeys();
+        base.OnModelCreating(modelBuilder);
+    }
+}
