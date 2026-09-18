931450c44e5b7f653812dc583f24705b8fc1ea0c
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2026-09-18 16:43:21 +0300

    Update AppUserRoleGroupService.cs

--- Karya.Core.Indentity/Services/AppUserRoleGroupService.cs
+++ Karya.Core.Indentity/Services/AppUserRoleGroupService.cs
@@ -1,6 +1,10 @@
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Data.ResponseModel;
 using Karya.Core.Indentity.Domains.Entities;
+using Karya.Core.Indentity.DTOs;
 using Karya.Core.Indentity.Infrastructure;
 using Karya.Core.Interfaces.Identities;
+using Karya.Core.Results;
 using Karya.Core.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,4 +14,21 @@ namespace Karya.Core.Indentity.Services;
 public class AppUserRoleGroupService : BaseService<AppUserRoleGroupRepository, AppUserRoleGroup, Guid>
 {
     public AppUserRoleGroupService(DbContext context, ICurrentUser currentUser) : base(new IdentityUnitOfWork(context, currentUser)) { }
+
+    public override async Task<BaseResult<LoadResult>> Select<TDto>(DataSourceLoadOptionsBase filterDataOptions)
+    {
+        var query = Query()
+            .Select(x => new AppUserRoleGroupLDto
+            {
+                Id = x.Id,
+                RoleGroupId = x.RoleGroupId,
+                RoleGroupName = x.RoleGroup.Name,
+                UserId = x.UserId,
+                UserName = x.User.UserName
+            });
+
+        var result = await DataSourceLoader.LoadAsync(query, filterDataOptions);
+
+        return BaseResult<LoadResult>.Success("200", null, result);
+    }
 }
